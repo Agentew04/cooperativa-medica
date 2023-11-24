4cd8245b171d0b6bdb9b82967434c83fd9c7cbb7
Rodrigo Appelt <31523279+Agentew04@users.noreply.github.com>
2023-11-24 09:56:49 -0300

    Add plan e bank collection

--- Models/Plan.cs
+++ Models/Plan.cs
@@ -4,7 +4,11 @@ public class Plan {
     public int Id { get; set; }
 
     public string Name { get; set; } = "";
-    public List<Service> Services {get;set;} = new();
     public float Discount {get;set;}
     public float Price {get;set;}
+
+    public async Task<List<Service>> GetServices() {
+        await Task.Delay(1);
+        return new();
+    }
 }
