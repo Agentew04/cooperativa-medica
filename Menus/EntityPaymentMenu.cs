733a30fcdd0e89b54c49312f7c582cfc910569c8
Rodrigo Appelt <31523279+Agentew04@users.noreply.github.com>
2023-11-28 01:20:20 -0300

    Menu Pagamento entidade afiliada

--- Menus/EntityPaymentMenu.cs
+++ Menus/EntityPaymentMenu.cs
@@ -1,31 +1,26 @@
 ﻿using CoopMedica.Database;
 using CoopMedica.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CoopMedica.Menus;
-public class ClientPaymentMenu : AbstractMenu {
-    protected override string Title => "Menu Pagamento Cliente";
+public class EntityPaymentMenu : AbstractMenu {
+    protected override string Title => "Menu Carnê de Pagamento (Entidade)";
 
-    private readonly ClientPaymentCollection clientPaymentCollection = new();
+    private readonly EntityPaymentCollection entityPaymentCollection = new();
 
-    private readonly ClientCollection clientCollection = new();
+    private readonly AffiliatedEntityCollection affiliatedEntityCollection = new();
 
     private readonly BankCollection bankCollection = new();
 
     protected override async Task Add() {
-        Console.WriteLine("==== Adicionar Pagamento Cliente ====");
-        int idCliente = Utils.ReadInt("Id do Cliente: ") ?? default;
-        if (!await clientCollection.Contains(x => x.Id == idCliente)) {
-            Utils.Print("Não existe cliente com este id!", ConsoleColor.Red);
+        Console.WriteLine("==== Adicionar Carnê de Pagamento (Entidade) ====");
+        int idCliente = Utils.ReadInt("Id da entidade afiliada: ") ?? default;
+        if (!await affiliatedEntityCollection.Contains(x => x.Id == idCliente)) {
+            Utils.Print("Não existe entidade afiliada com este id!", ConsoleColor.Red);
             return;
         }
 
         int idBanco = Utils.ReadInt("Id do Banco: ") ?? default;
-        
+
         if (!await bankCollection.Contains(x => x.Id == idBanco)) {
             Utils.Print("Não existe banco com este id!", ConsoleColor.Red);
             return;
@@ -33,36 +28,36 @@ public class ClientPaymentMenu : AbstractMenu {
 
         double valor = Utils.ReadDouble("Valor: ");
 
-        ClientPayment clientPayment = new() {
-            ClientId = idCliente,
+        EntityPayment clientPayment = new() {
+            EntityId = idCliente,
             BankId = idBanco,
             Amount = (float)valor
         };
 
-        await clientPaymentCollection.AddAsync(clientPayment);
+        await entityPaymentCollection.AddAsync(clientPayment);
         Utils.Print("Pagamento do Cliente adicionado com sucesso!", ConsoleColor.Green);
     }
 
     protected override async Task Edit() {
-        Console.WriteLine("==== Editar Pagamento do Cliente ====");
+        Console.WriteLine("==== Editar Carnê de Pagamento (Entidade) ====");
 
         Console.WriteLine("Digite o id do pagamento: ");
         int idPagamento = Utils.ReadInt("> ", false) ?? default;
-        if (!await clientPaymentCollection.Contains(x => x.Id == idPagamento)) {
+        if (!await entityPaymentCollection.Contains(x => x.Id == idPagamento)) {
             Utils.Print("Não existe pagamento com este id!", ConsoleColor.Red);
             return;
         }
 
-        ClientPayment payment = (await clientPaymentCollection.SelectOneAsync(x => x.Id == idPagamento))!;
-        int idCliente = Utils.ReadInt("Id do Cliente: ", defaultValue: payment.ClientId) ?? default;
-        if (!await clientCollection.Contains(x => x.Id == idCliente)) {
-            Utils.Print("Não existe cliente com este id!", ConsoleColor.Red);
+        EntityPayment payment = (await entityPaymentCollection.SelectOneAsync(x => x.Id == idPagamento))!;
+        int entityId = Utils.ReadInt("Id da Entidade: ", defaultValue: payment.EntityId) ?? default;
+        if (!await affiliatedEntityCollection.Contains(x => x.Id == entityId)) {
+            Utils.Print("Não existe entidade afiliada com este id!", ConsoleColor.Red);
             return;
         }
-        payment.ClientId = idCliente;
+        payment.EntityId = entityId;
 
         int idBanco = Utils.ReadInt("Id do Banco: ", defaultValue: payment.BankId) ?? default;
-        if(!await bankCollection.Contains(x => x.Id == idBanco)) {
+        if (!await bankCollection.Contains(x => x.Id == idBanco)) {
             Utils.Print("Não existe banco com este id!", ConsoleColor.Red);
             return;
         }
@@ -70,34 +65,34 @@ public class ClientPaymentMenu : AbstractMenu {
 
         payment.Amount = (float)Utils.ReadDouble("Valor: ", defaultValue: payment.Amount);
 
-        await clientPaymentCollection.UpdateAsync(payment);
+        await entityPaymentCollection.UpdateAsync(payment);
         Utils.Print("Dependente editado com sucesso!", ConsoleColor.Green);
     }
 
     protected override async Task List() {
-        Console.WriteLine("==== Listar Pagamento dos Clientes ====");
-        IEnumerable<ClientPayment> payments = await clientPaymentCollection.SelectAsync();
+        Console.WriteLine("==== Listar Carnês de Pagamento (Entidades) ====");
+        IEnumerable<EntityPayment> payments = await entityPaymentCollection.SelectAsync();
 
-        
-        Table<ClientPayment> paymentsTable = new();
+
+        Table<EntityPayment> paymentsTable = new();
         paymentsTable.RegisterColumn(name: "Id", function: x => x.Id.ToString())
-            .RegisterColumn(name: "Id Cliente", function: x => x.ClientId.ToString())
+            .RegisterColumn(name: "Id Entidade", function: x => x.EntityId.ToString())
             .RegisterColumn(name: "Id Banco", function: x => x.BankId.ToString())
             .RegisterColumn(name: "Valor", function: x => x.Amount.ToString("C2"));
         paymentsTable.AddRows(payments);
 
         if (!payments.Any()) {
-            Utils.Print("Não existem pagamentos cadastrados", ConsoleColor.Red);
+            Utils.Print("Não existem carnês de pagamento cadastrados", ConsoleColor.Red);
             return;
         }
         paymentsTable.DisplayTable();
     }
 
     protected override async Task Remove() {
-        Console.WriteLine("==== Remover Pagamento do Cliente ====");
+        Console.WriteLine("==== Remover Carnê de Pagamento (Entidade) ====");
         Console.WriteLine("Digite o id do pagamento: ");
         int idCliente = Utils.ReadInt("> ", false) ?? default;
-        int removed = await clientPaymentCollection.RemoveAsync(x => x.Id == idCliente);
+        int removed = await entityPaymentCollection.RemoveAsync(x => x.Id == idCliente);
         if (removed > 0) {
             Utils.Print("Pagamento removido com sucesso!", ConsoleColor.Green);
         } else {
